24d01832133e9fa5be19cffb093dbacf89208c9d
Oliver <olibadescu@gmail.com>
2025-08-31 15:02:02 +0300

    Add factory pattern example

---
+++ src/factory_pattern/example/Car.java
@@ -0,0 +1,18 @@
+package factory_pattern.example;
+
+public class Car extends Vehicle {
+    @Override
+    public String startEngine() {
+        return "Car engine started";
+    }
+    
+    @Override
+    public String stopEngine() {
+        return "Car engine stopped";
+    }
+    
+    @Override
+    public String getInfo() {
+        return "This is a car";
+    }
+}
